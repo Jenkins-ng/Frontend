bedc26b8985cc8e22d62f0d789a46ed435054e1f
Miracle <miraclepatidehen@gmail.com>
2023-07-07 16:55:58 +0100

    moved AuthContext to context folder

---
+++ src/context/AuthContext.jsx
@@ -0,0 +1,31 @@
+import { createContext, useContext, useState, useEffect } from 'react'
+import api from '../utils/api'
+
+const AuthContext = createContext(null)
+
+// custom hook
+export const useAuth = () => useContext(createContext)
+
+const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null)
+
+  useEffect(() => {
+    // check if user exists here
+  }, [])
+
+  const login = (user) => {
+    setUser(user)
+  }
+
+  const logout = (user) => {
+    setUser(null)
+  }
+
+  return (
+    <AuthContext.Provider value={{ user, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  )
+}
+
+export default AuthProvider
