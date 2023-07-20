6480baa6218b4a00ecd686da6b0d00731dc2e44c
Miracle <miraclepatidehen@gmail.com>
2023-07-20 15:44:23 +0100

    added apiPrivate

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,11 +1,14 @@
 import axios from 'axios'
+const base_url = 'https://api.jenkins.ng/api/v1'
 
 const api = axios.create({
-  baseURL: 'https://api.jenkins.ng/api/v1',
-  headers: {
-    'Content-Type': 'application/json',
-    withCredentials: true,
-  },
+  baseURL: base_url,
+})
+
+export const apiPrivate = axios.create({
+  baseURL: base_url,
+  headers: { 'Content-Type': 'application/json' },
+  withCredentials: true,
 })
 
 export default api
