d77c3a97ea918d77d798e1a5560ef73a10374942
jybium <asaolujtomi@gmail.com>
2023-08-20 13:56:27 +0100

    Fix merge conflict

--- src/components/Landing page/Reviews/Reviews.jsx
+++ src/components/Landing page/Reviews/Reviews.jsx
@@ -1,12 +1,11 @@
-import { Divider } from '@mui/material'
-import React from 'react'
-import '@coreui/coreui/dist/css/coreui.min.css'
+import { Divider } from "@mui/material";
+import React from "react";
+import "@coreui/coreui/dist/css/coreui.min.css";
 import {
   CCarousel,
   CCarouselCaption,
   CCarouselItem,
   CImage,
-<<<<<<< HEAD
 } from "@coreui/react";
 import CarouselText from "../../../utils/CarouselText";
 import Image from "../../../assets/Team/jybium.jpg";
@@ -39,15 +38,6 @@ export const Images = () => {
     </div>
   );
 };
-=======
-} from '@coreui/react'
-import CarouselText from '../../../utils/CarouselText'
-import Image from '../../../assets/Team/jybium.jpg'
-
-export const Images = () => {
-  return <img src={Image} alt="" />
-}
->>>>>>> 3baf41f90921901822b6980931a1ecff3376baf2
 const Reviews = () => {
   return (
     <section>
@@ -103,7 +93,7 @@ const Reviews = () => {
         </CCarousel> */}
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Reviews
+export default Reviews;
